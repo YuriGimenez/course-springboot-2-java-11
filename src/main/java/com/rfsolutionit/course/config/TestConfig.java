6fecb8ab948c0aa4ed7fa682ba930440b6c43410
Yuri Gimenez <yuri.gimenez23@gmail.com>
2022-03-08 16:42:03 -0300

    Category

--- src/main/java/com/rfsolutionit/course/config/TestConfig.java
+++ src/main/java/com/rfsolutionit/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.rfsolutionit.course.entities.Category;
 import com.rfsolutionit.course.entities.Order;
 import com.rfsolutionit.course.entities.User;
 import com.rfsolutionit.course.entities.enums.OrderStatus;
+import com.rfsolutionit.course.repositories.CategoryRepository;
 import com.rfsolutionit.course.repositories.OrderRepository;
 import com.rfsolutionit.course.repositories.UserRepository;
 
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
